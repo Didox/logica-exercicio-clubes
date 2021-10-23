55ab07090acc2737adc7a784415d84a6b0ca7c75
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2021-10-23 18:04:53 -0300

    novo exercicio

--- Jogo.cs
+++ Jogo.cs
@@ -8,5 +8,11 @@ namespace clubes_futebol
         public Clube Time2 {get;set;}
         public int ResultadoTime1 {get;set;}
         public int ResultadoTime2 {get;set;}
-    }
+
+        public string Vencedor()
+        {
+            if(ResultadoTime1 == ResultadoTime2) return "O resultado foi Empate";
+            return $"O ganhador do jogo foi { ((ResultadoTime1 > ResultadoTime2) ? Time1.Nome : Time2.Nome )}";
+        }
+  }
 }
--- Program.cs
+++ Program.cs
@@ -60,6 +60,51 @@ namespace clubes_futebol
                 }
             }
 
+
+            /*
+            * Josevaldo, é dono de uma cadeia de fast food' 
+            * faça um programa que cadastre os ingredientes de um hamburger
+            * que cadastre um hambuger selecionando os ingredientes
+            * faça um pedido para um cliente
+            * selecionando os hambugers
+            * no final do programa, mostre um relatório com os clientes e seus pedidos
+            * com o detalhe sobre tudo, mostrando os hamburgers e os detalhes do hamburger
+            Ex:
+
+            Pedido do(a) XXX
+            Ele escolheu os hamburgers
+                - Gold Premim
+                    - Salada
+                    - Molho
+                    - Carne
+                    - Picles
+                - Prata Gerge
+                    - Salada
+                    - Molho
+                    - Carne
+                    - Gergelim
+            Valor total de R$ 200 reais
+            ----------------------------------
+            Pedido do(a) YYY
+            Ele escolheu os hamburgers
+                - Gold Premim
+                    - Salada
+                    - Molho
+                    - Carne
+                    - Picles
+                - Prata Gerge
+                    - Salada
+                    - Molho
+                    - Carne
+                    - Gergelim
+                - Prata Gerge
+                    - Salada
+                    - Molho
+                    - Carne
+                    - Gergelim
+            Valor total de R$ 200 reais
+            */
+
         }
 
         private static void listarJogos(List<Jogo> jogos)
@@ -70,6 +115,8 @@ namespace clubes_futebol
             else{
                 foreach(var jogo in jogos){
                     Console.WriteLine($"{jogo.Time1.Nome} ({jogo.ResultadoTime1}) vs ({jogo.ResultadoTime2}) {jogo.Time2.Nome}");
+                    Console.WriteLine("-------------------------------");
+                    Console.WriteLine(jogo.Vencedor());
                 }
             }
 
